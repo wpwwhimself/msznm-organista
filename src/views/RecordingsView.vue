d4cb352518f1ddf35741c231fa8844bafa5c13f7
Wojciech Przybyła <contact@wpww.pl>
2024-09-05 23:39:05 +0200

    💬 added new materials to recordings

--- src/views/RecordingsView.vue
+++ src/views/RecordingsView.vue
@@ -1,5 +1,34 @@
+<script setup lang="ts">
+import ImagedShowcase from '../components/ImagedShowcase.vue';
+
+const showcases = [
+  {
+    title: "Shorty",
+    content: "Krótkie nagrania, jakie udaje mi się uchwycić podczas gry.",
+    link: "https://www.youtube.com/playlist?list=PLpS8kGlqkYvS3nI6F_S2ZMchY3oS8VgjU",
+    img: undefined,
+  },
+  {
+    title: "Gig Vlogi",
+    content: "Dłuższe relacje z grań z moimi komentarzami.",
+    link: "https://www.youtube.com/playlist?list=PLpS8kGlqkYvQAj957C4JmjMwXmFSa9Z4_",
+    img: undefined,
+  },
+]
+</script>
+
 <template>
-  tu mam udokumentowane, jak ładnie gram
+  <p>Tu mam udokumentowane, jak gram:</p>
+
+  <div class="flex-right justify-center wrap">
+    <ImagedShowcase v-for="showcase in showcases" :key="showcase.title"
+      :title="showcase.title"
+      :img="showcase.img"
+    >
+      <p>{{ showcase.content }}</p>
+      <a target="_blank" :href="showcase.link">Link</a>
+    </ImagedShowcase>
+  </div>
 </template>
 
 <style>
